3d53b81f4fb64fe2736981ae9113ae470f244475
suehiro41523 <67720039+suehiro41523@users.noreply.github.com>
2023-12-30 17:20:05 +0900

    connect vue and laravel frontend

--- src/LaravelTestProject/routes/web.php
+++ src/LaravelTestProject/routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Auth;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,8 +14,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/', function ()
+{
+    return view('app');
 });
 
 Auth::routes();
