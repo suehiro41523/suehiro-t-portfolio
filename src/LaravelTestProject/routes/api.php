362d9bba5739498ff8a8153d0144956e6684f8f5
suehiro41523 <67720039+suehiro41523@users.noreply.github.com>
2024-01-07 16:40:38 +0900

    update basic database

--- src/LaravelTestProject/routes/api.php
+++ src/LaravelTestProject/routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\api\V1\WorkController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -14,6 +15,12 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+Route::middleware('auth:sanctum')->get('/user', function (Request $request)
+{
     return $request->user();
+});
+
+Route::group(['prefix' => 'v1'], function ()
+{
+    Route::apiResource('works', WorkController::class);
 });
